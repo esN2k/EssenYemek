d84dd6a86f47e00dcaa622ac4db1e69d90f0c4e8
Doğukan Balaman <dodikbalaman@gmail.com>
2025-12-30 20:07:03 +0300

    chore(firebase): add flutterfire config

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -1,17 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import '/firebase_options.dart';
 
 Future initFirebase() async {
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyBDYSF21Y61rdLH42jxRgXeUceGaDxmAYQ",
-            authDomain: "essen-yemek-79lvwr.firebaseapp.com",
-            projectId: "essen-yemek-79lvwr",
-            storageBucket: "essen-yemek-79lvwr.firebasestorage.app",
-            messagingSenderId: "188409565395",
-            appId: "1:188409565395:web:41f756cf62982d98fe0426"));
-  } else {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
